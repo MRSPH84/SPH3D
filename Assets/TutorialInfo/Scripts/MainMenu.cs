c164c76c817db3ecbd1bef100f50bd14c2ab233d
MRSPH84 <sepehradamirian@gmail.com>
2025-07-04 14:23:03 +0330

    MRSPH V4

---
+++ Assets/TutorialInfo/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("YourGameplaySceneName"); // اسم صحنه اصلی رو بذار
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game...");
+        Application.Quit();
+    }
+}
