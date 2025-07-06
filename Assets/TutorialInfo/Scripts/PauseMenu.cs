8d52a197901ac3f4e70b6189a473bb3eeb1eb7ca
MRSPH84 <sepehradamirian@gmail.com>
2025-07-07 00:42:25 +0330

    MR SPH V9

---
+++ Assets/TutorialInfo/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public bool isPaused = false;
+
+    void Start()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Quit Game...");
+        Application.Quit(); // در ادیتور جواب نمیده، ولی در بیلد کار میکنه
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu"); // اگه منوی اصلی داری
+    }
+}
