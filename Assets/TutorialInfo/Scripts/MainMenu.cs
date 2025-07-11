04361acba315b963afbbf2d6efbbbbcf051aabb8
MRSPH84 <sepehradamirian@gmail.com>
2025-07-11 13:04:49 +0330

    V15

--- Assets/TutorialInfo/Scripts/MainMenu.cs
+++ Assets/TutorialInfo/Scripts/MainMenu.cs
@@ -3,14 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    [Header("Scene Settings")]
+    public string sceneToLoad = "SampleScene"; // یا اسم صحنه اصلی‌ات
+
+    // شروع بازی
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene"); // اسم صحنه اصلی رو بذار
+        SceneManager.LoadScene(sceneToLoad);
     }
 
+    // خروج از بازی
     public void ExitGame()
     {
         Debug.Log("Exiting Game...");
         Application.Quit();
+    }
+
+    // باز کردن تنظیمات
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    // برگشت به منو
+    public void BackToMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
 }
---
+++ Assets/TutorialInfo/Scripts/SettingsMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [Header("UI Components")]
+    public Slider volumeSlider;
+    public Dropdown qualityDropdown;
+
+    void Start()
+    {
+        // مقدار اولیه اسلایدر صدا
+        volumeSlider.value = PlayerPrefs.GetFloat("volume", 0.75f);
+        AudioListener.volume = volumeSlider.value;
+
+        // مقدار کیفیت گرافیک
+        int qualityIndex = PlayerPrefs.GetInt("quality", 2);
+        qualityDropdown.value = qualityIndex;
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+
+    public void SetGraphicsQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+        PlayerPrefs.SetInt("quality", index);
+    }
+}
